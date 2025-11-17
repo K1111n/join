aab5dacf37a758ee7fddaa15188b158d287c4e2e
Kajanan Yoganathan <kaja_nan@hotmail.de>
2025-11-17 11:13:00 +0100

    feat: cloned projekt and implemented my own firebase project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,13 +10,20 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'join-554eb',
-        appId: '1:949596014477:web:7c0328626b1faaf4fadfc4',
-        storageBucket: 'join-554eb.firebasestorage.app',
-        apiKey: 'AIzaSyDFn8HmBoR5x0S2UnGjaGzxfoxy_Pd5BoA',
-        authDomain: 'join-554eb.firebaseapp.com',
-        messagingSenderId: '949596014477',
-        measurementId: 'G-1DYD7G0MD3',
+        // projectId: 'join-554eb',
+        // appId: '1:949596014477:web:7c0328626b1faaf4fadfc4',
+        // storageBucket: 'join-554eb.firebasestorage.app',
+        // apiKey: 'AIzaSyDFn8HmBoR5x0S2UnGjaGzxfoxy_Pd5BoA',
+        // authDomain: 'join-554eb.firebaseapp.com',
+        // messagingSenderId: '949596014477',
+        // measurementId: 'G-1DYD7G0MD3',
+        apiKey: "AIzaSyD3VUdkbpIflpLQH8H0a7-8KVfVYdX18lI",
+  authDomain: "join-3ba5a.firebaseapp.com",
+  projectId: "join-3ba5a",
+  storageBucket: "join-3ba5a.firebasestorage.app",
+  messagingSenderId: "560891714218",
+  appId: "1:560891714218:web:6740a32aaf22ab4ca9ced3",
+  measurementId: "G-4B4FS7FVGQ"
       })
     ),
     provideFirestore(() => getFirestore()),
